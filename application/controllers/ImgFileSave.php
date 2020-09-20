c2fd92560e994d69c18f874e3f882b74825ca733
Bidyut Debnath <1992bidyut@gmail.com>
2020-09-20 22:41:13 +0600

    duplicate payment rejection

--- application/controllers/ImgFileSave.php
+++ application/controllers/ImgFileSave.php
@@ -19,20 +19,20 @@
 class ImgFileSave extends CI_Controller
 {
 	private $email = "";
-	
-	function __construct() 
+
+	function __construct()
 	{
-		parent::__construct();				
-		$this->isSuccessfulLogin = false;		
+		parent::__construct();
+		$this->isSuccessfulLogin = false;
 		$this->load->helper('url');
 		$this->load->model('login_model');
 		$this->load->model('payment_model');
 	}
-		
+
 	 protected $rest_format   = 'application/json';
-	 
+
 	 //protected $methods              = array('HTTP/1.1');
-	
+
 	public function index()
 	{
 		 if(empty($_POST['username']) || !isset($_POST['password']))
@@ -60,53 +60,53 @@ class ImgFileSave extends CI_Controller
 				$fileType = $_POST['image_type'];
 				//$order_code=$_POST['order_code'];
 				$payment_id=$_POST['payment_id'];
-				
+
 				$userData=$this->login_model->getUser($username,sha1($password));
 				$user_id=$userData[0]['id'];
 				$clientData=$this->login_model->getClientUserInfo($user_id);
 			    $client_id=$clientData[0]['client_id'];
-			
-				$server_url= dirname(__FILE__); 
+
+				$server_url= dirname(__FILE__);
 				// die($server_url);
 				//$folder_path = $server_url.'//..//..//..//recorder.onuserver.com';
-//				 $folder_path = '/home/demoonuserver/public_html/payFlex/asset/images';//demo server
-				$folder_path = '/srv/users/serverpilot/apps/payflex/public/total/asset/images';//live sev=rver
-				//check if is_dir returns valid path for our folder path so far. 
-				
-				$file_path = $folder_path .'/'. ((string)$client_id);	
+				 $folder_path = '/home/demoonuserver/public_html/payFlex/asset/images';//demo server
+//				$folder_path = '/srv/users/serverpilot/apps/payflex/public/total/asset/images';//live sev=rver
+				//check if is_dir returns valid path for our folder path so far.
+
+				$file_path = $folder_path .'/'. ((string)$client_id);
 
 				//create user name filepath and check if already exists, otherwise create
 
 				//Checking if folder already exist----------------------------------------
-				if (!file_exists($file_path )) 
+				if (!file_exists($file_path ))
 				{
 					mkdir($file_path , 0777, true);
-					
+
 				}
 
-				//create device id file path , check already exists and then create if necessary. 
+				//create device id file path , check already exists and then create if necessary.
 				$devide = explode('.',$_FILES['uploaded_file']['name']);
 				$uploadTo = $file_path .'/'. $trxid . '.'.$devide[1];
-					
+
 				$resonseStatus = 4400 ;
 				$resonseReason = 'Failed to upload' ;
 
 				$responseContent=array();
-				
-				
-				if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$uploadTo))	
-				{	
+
+
+				if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$uploadTo))
+				{
 					$resonseStatus = 202 ;
 					$request_file_size = $_FILES['uploaded_file']['size'];
 					$file_size = filesize($uploadTo);
 					$fileExists = file_exists($uploadTo);
-					
+
 					$imgInfo['trxid']=$trxid;
 					$imgInfo['image_name']=$trxid.'.'.$devide[1];
 					$imgInfo['user_id']=$userID;
 					$imgInfo['request_time']=$request_time;
 					$imgInfo['upload_time']=$request_time;
-					$imgInfo['image_discription']=$file_detail;				
+					$imgInfo['image_discription']=$file_detail;
 					$imgInfo['image_type_id']=$fileType;
 
 					$relation['image_id']=$this->payment_model->saveClientImagInfo($imgInfo);
@@ -139,19 +139,19 @@ class ImgFileSave extends CI_Controller
 					$responseContent['file_size'] = 0;
 					$responseContent['file_exists'] = false;
 				}
-				
+
 				//array_push($sms_array,$responseContent);
 				$myJSonDatum = json_encode($responseContent);
-				
+
 			 }
 			 catch(Exception $ex)
 			 {
-			 }	
+			 }
 		 }
 		 echo $myJSonDatum;
 		 //$this->response($myJSonDatum, 202);
 		 //print_r($myJSonDatum); // OK (200) being the HTTP response code
 		// die;
 	}
-	
+
 }
